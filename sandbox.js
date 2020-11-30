5bf3953883524c89aa3a6628504a00b199e7fda2
Ryan Etten <retten21127@columbushs.com>
2020-11-30 17:47:19 -0500

    video 4.33

--- sandbox.js
+++ sandbox.js
@@ -1,12 +1,44 @@
-// returning values
+// regular function
+// const calcArea = function(radius){
+//   return 3.14 * radius**2;
+// };
 
-// const speak = function(name = 'luigi', time = 'night'){
-//   console.log(`good ${time} ${name}`);
+// arrow function
+// const calcArea = radius => 3.14 * radius**2;
+
+
+
+// const area = calcArea(5);
+// console.log('area is:', area);
+
+
+
+
+
+// practice arrow functions
+
+// const greet = function(){
+//   return 'hello, world';
 // }
 
-const calcArea = function(radius){
-  return 3.14 * radius**2;
+// const greet = () => 'hello world';
+// const result = greet();
+// console.log(result);
+
+// const bill = function(products, tax){
+//   let total = 0;
+//   for(let i = 0; i < products.length; i++){
+//     total += products[i] + products[i] * tax;
+//   }
+//   return total;
+// }
+
+const bill = (products, tax) => {
+  let total = 0;
+  for(let i = 0; i < products.length; i++){
+    total += products[i] + products[i] * tax;
+  }
+  return total;
 };
 
-const area = calcArea(5);
-console.log(area);
+console.log(bill([10,15,30], 0.2))
